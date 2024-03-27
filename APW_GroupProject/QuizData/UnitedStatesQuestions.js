f140ccac3b247a885fe9e6cdf319359f3c367e16
VitaleCoding <144835102+VitaleCoding@users.noreply.github.com>
2024-03-27 16:48:29 -0400

    Add files via upload

---
+++ APW_GroupProject/QuizData/UnitedStatesQuestions.js
@@ -0,0 +1,253 @@
+const unitedStatesQuizQuestions = [
+    {
+        //1
+        question: "Who was the first President of the United States?",
+        options: ["George Washington", "Thomas Jefferson", "Abraham Lincoln", "John Adams"],
+    },
+    {
+        //2
+        question: "What year did the American Revolutionary War begin?",
+        options: ["1775", "1783", "1776", "1789"],
+    },
+    {
+        //3
+        question: "The First Amendment grants the right to:",
+        options: ["Freedom of Speech", "Right to Bear Arms", "No Search or Seuizure Without Warrant", "Prohibition of Alcohol"],
+    },
+    {
+        //4
+        question: "What year was Plymouth settled?",
+        options: ["1607", "1585", "1620", "1643"],
+    },
+    {
+        //5
+        question: "What was the first English settlment in America?",
+        options: ["Roanoke", "Jamestown", "Plymouth", "Salem"],
+    },
+    {
+        //6
+        question: "What was the first *successful* English settlement in America?",
+        options: ["Roanoke", "Jamestown", "Plymouth", "Salem"],
+    },
+    {
+        //7
+        question: "What year was Jamestown settled?",
+        options: ["1620", "1585", "1607", "1643"],
+    },
+    {
+        //8
+        question: "What year was Salem settled?",
+        options: ["1643", "1607", "1620", "1585"],
+    },
+    {
+        //9
+        question: "Who was the general responsible for winning the Civil War?",
+        options: ["Douglas MacArthur", "Andrew Jackson", "Ulysses S. Grant", "George S. Patton"],
+    },
+    {
+        //10
+        question: "Who was the King of England during the American Revolution?",
+        options: ["Henry VIII", "James I", "George III", "George IV"],
+    },
+    {
+        //11
+        question: "When was the Declaration of Independence ratified?",
+        options: ["July 4th 1776", "December 15th 1791", "January 1st 1863", "April 9th 1865"],
+    },
+    {
+        //12
+        question: "The ____ Amendment abolished slavery",
+        options: ["11th", "14th", "15th", "13th"],
+    },
+    {
+        //13
+        question: "Who assassinated Abraham Lincoln?",
+        options: ["Aaron Burr", "Lee Harvey Oswald", "Ted Kaczynski ", "John Wilkes Booth"],
+    },
+    {
+        //14
+        question: "Who assassinated John F. Kennedy?",
+        options: ["Aaron Burr", "Lee Harvey Oswald", "Ted Kaczynski ", "John Wilkes Booth"],
+    },
+    {
+        //15
+        question: "Who escaped slavery and went on to help free over 300 slaves in the Underground Railroad?",
+        options: ["Rosa Parks", "Harriet Tubman", "Sojourner Truth", "Frederick Douglass"],
+    },
+    {
+        //16
+        question: "Who began the Civil Rights movement of 1954?",
+        options: ["Huey P. Newton", "Dred Scot", "Martin Luther King, Jr.", "George Washington Carver"],
+    },
+    {
+        //17
+        question: "What president WASN'T in office during the Vietnam War?",
+        options: ["John F. Kennedy", "Richard Nixon", "Lyndon B. Johnson", "Jimmy Carter"],
+    },
+    {
+        //18
+        question: "Who did President Truman choose to lead the USA during the Korean War?",
+        options: ["Phil Sheridan", "Douglas MacArthur", "Matthew Ridgway", "George Patton"],
+    },
+    {
+        //19
+        question: "When was the Patriot Act passed?",
+        options: ["October 26th 2001", "June 3nd 2015", "September 11th 2001", "January 4th 2011"],
+    },
+    {
+        //20
+        question: "When was the Alamo?",
+        options: ["1836", "1843", "1833", "1820"],
+    },
+    {
+        //21
+        question: "What Amendment banned manufacture, sale, and transport of all alcohol?",
+        options: ["17th", "5th", "18th", "9th"],
+    },
+    {
+        //22
+        question: "Which treaty ended the French and Indian War?",
+        options: ["Treaty of Paris 1783", "Treaty of Paris 1763", "Gettysburg Address", "The Great Compromise"],
+    },
+    {
+        //23
+        question: "Which treaty ended the Revolutionary War?",
+        options: ["Treaty of Paris 1763", "Treaty of Paris 1783", "Gettysburg Address", "The Great Compromise"],
+    },
+    {
+        //24
+        question: "Who designed the first American Flag?",
+        options: ["Betsy Ross", "Abigail Adams", "Mercy Warren", "Molly Pitcher"],
+    },
+    {
+        //25
+        question: "Who was the 26th President?",
+        options: ["Warren Harden", "William McKinley", "Howard Taft", "Theodore Roosevelt"],
+    },
+    {
+        //26
+        question: "What year was the Panama Canal finished?",
+        options: ["1904", "1912", "1908", "1914"],
+    },
+    {
+        //27
+        question: "Who was the 30th president?",
+        options: ["Warren Harding", "Calvin Coolidge", "Herbert Clark Hoover", "Woodrow Wilson"],
+    },
+    {
+        //28
+        question: "When was the United Nations formed?",
+        options: ["1945", "1940", "1949", "1955"],
+    },
+    {
+        //29
+        question: "When was NATO formed?",
+        options: ["1945", "1949", "1950", "1943"],
+    },
+    {
+        //30
+        question: "What year did the Vietnam War end?",
+        options: ["1959", "1963", "1975", "1972"],
+    },
+    {
+        //31
+        question: "Where was the first atomic bomb tested?",
+        options: ["Hiroshima, Japan", "Trinity, New Mexico", "Nagasaki, Japan", "Los Alamos, New Mexico"],
+    },
+    {
+        //32
+        question: "What Native American tribe did Pocahontas come from?",
+        options: ["Cherokee", "Lenape", "Shawnee", "Powhatan"],
+    },
+    {
+        //33
+        question: "How many people were executed for witchcraft in Salem?",
+        options: ["20", "22", "30", "25"],
+    },
+    {
+        //34
+        question: "What tariff was not a tariff imposed upon the American colonies by the British?",
+        options: ["The Sugar Act", "The Stamp Act", "The Molasses Act", "The Water Act"],
+    },
+    {
+        //35
+        question: "Who invented bifocals?",
+        options: ["Baron Philip Von Stosch", "Benjamin Franklin", "Otto Wichterle", "Salvino D'Armate"],
+    },
+    {
+        //36
+        question: "What was the first American State?",
+        options: ["Delaware", "Maine", "Pennsylvania", "Maryland"],
+    },
+    {
+        //37
+        question: "Which president was the first president to officially declare war?",
+        options: ["George Washington", "Andrew Jackson", "Theodore Roosevelt", "James Madison"],
+    },
+    {
+        //38
+        question: "What war did president James Madison start?",
+        options: ["The Revolutionary War", "The War of 1812", "The French and Indian War", "The Civil War"],
+    },
+    {
+        //39
+        question: "What was recorded as 'the bloodiest battle of American history'?",
+        options: ["The Alamo", "The Battle of Antietam", "The Battle of Gettysburg", "The Battle of Chancellorsville"],
+    },
+    {
+        //40
+        question: "Which Native American tribe walked the Trail of Tears?",
+        options: ["Cherokee", "Shawnee", "Powhatan", "Lenape"],
+    },
+    {
+        //41
+        question: "Who was the first President to be impeached?",
+        options: ["Donald J. Trump", "Bill Clinton", "Richard Nixon", "Andrew Johnson"],
+    },
+    {
+        //42
+        question: "When was the very first US dollar printed?",
+        options: ["1860", "1864", "1863", "1862"],
+    },
+    {
+        //43
+        question: "When did the United States drop the gold standard?",
+        options: ["1975", "1971", "1970", "1968"],
+    },
+    {
+        //44
+        question: "What country gifted the Statue of Liberty to the United States?",
+        options: ["Great Britain", "Canada", "Mexico", "France"],
+    },
+    {
+        //45
+        question: "Who was the first man on the moon?",
+        options: ["John Glenn", "Neil Armstrong", "Buzz Aldrin", "Scott Kelly"],
+    },
+    {
+        //46
+        question: "When did the Great Depression end?",
+        options: ["1930", "1931", "1925", "1929"],
+    },
+    {
+        //47
+        question: "What was the first capitol of the United States?",
+        options: ["New York City, New York", "York, Pennsylvania", "Lancaster, Pennsylvania", "Washington, D.C."],
+    },
+    {
+        //48
+        question: "What is the current capitol of the United States?",
+        options: ["York, Pennsylvania", "New York City, New York", "Lancaster, Pennsylvania", "Washington, D.C."],
+    },
+    {
+        //49
+        question: "How many people died during the Boston Massacre?",
+        options: ["3", "4", "5", "6"],
+    },
+    {
+        //50
+        question: "What was the name of the Pilgrim's ship?",
+        options: ["The Pinta", "The Nina", "The Santa Maria", "The Mayflower"],
+    },
+];
+module.exports = unitedStatesQuizQuestions;

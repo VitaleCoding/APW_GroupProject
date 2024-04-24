ffcd4fa2b6a7cd5b34d3fc9f0a2614f41ced1682
Spencer Hoban <shoban2005@gmail.com>
2024-04-24 17:07:45 -0400

    Added database_manager.js

---
+++ APW_GroupProject/database_manager.js
@@ -0,0 +1,56 @@
+const MongoClient = require("mongodb").MongoClient;
+const db_url = "mongodb://96.235.141.82:27017";
+let active_database;
+let database = {}
+
+const mongo_client = new MongoClient(db_url);
+
+// Internal function for connecting to database. Use database.get instead
+var connect = async function(database_name) {
+    try {
+        // Await connection to client
+        await mongo_client.connect();
+
+        // Once connection established, attempt to get wanted database
+        active_database = mongo_client.db(database_name);
+
+        // If client fails to return wanted database, error
+        if (!active_database) {
+            throw new Error(`Failed to connect to database ${database_name}`);
+        }
+        // Otherwise return the database
+        else {
+            console.log(`Successfully connected to ${database_name}`);
+            return active_database;
+        }
+    }
+    catch(e) {
+        console.log(e.message);
+    }
+}
+
+// Get database
+// 'database_name' is only required for initial connection
+database.get = function(database_name) {
+    // Get database if already connected
+    if (active_database) {
+        return active_database;
+    }
+    // Otherwise attempt to connect to it
+    else {
+        return connect(database_name);
+    }
+}
+
+// Attempt to close database connection
+database.close = async function() {
+    try {
+        await mongo_client.close();
+        return;
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+}
+
+module.exports = database;

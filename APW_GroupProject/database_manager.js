6c9cd29483ab2a231a46f5f70edb867dcd725535
Spencer Hoban <shoban2005@gmail.com>
2024-04-24 18:32:33 -0400

    Changed database url to localhost

--- APW_GroupProject/database_manager.js
+++ APW_GroupProject/database_manager.js
@@ -1,5 +1,5 @@
 const MongoClient = require("mongodb").MongoClient;
-const db_url = "mongodb://96.235.141.82:27017";
+const db_url = "mongodb://127.0.0.1:27017";
 let active_database;
 let database = {}
 

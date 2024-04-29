c8db793a9ff63d537955991cc921abf1ed42574a
Spencer Hoban <shoban2005@gmail.com>
2024-04-29 17:25:18 -0400

    Added leaderboard functionality

--- APW_GroupProject/user_manager.js
+++ APW_GroupProject/user_manager.js
@@ -62,17 +62,12 @@ let format_user_doc = async function(user_doc) {
 
 // Attempt to retrieve user using username and password. Errors if either are incorrect/missing
 user_manager.authenticate_user = async function(username, password) {
-    try {
-        let user = await user_collection.findOne({"username": username});
+    let user = await user_collection.findOne({"username": username});
 
-        if (!user) throw new Error("User not found");
-        if (user.password != password) throw new Error("Password incorrect");
+    if (!user) throw new Error("User not found");
+    if (user.password != password) throw new Error("Password incorrect");
 
-        return format_user_doc(user);
-    }
-    catch (e) {
-        console.log(e.message);
-    }
+    return format_user_doc(user);
 }
 
 // Create a new user on the database
@@ -84,6 +79,107 @@ user_manager.create_user = async function(username, password) {
     });
 
     return result;
+}
+
+// Return user. If no user exists, create new user and return it instead
+user_manager.register_user = async function(username, password) {
+    // Attempt to find user
+    let user = null;
+    try {
+        user = await user_manager.authenticate_user(username, password);
+    }
+    catch (e) {
+        // If user not found, create a new user
+        // Otherwise rethrow error created
+        console.log(e)
+        if (e.message == "User not found") {
+            console.log(`Creating new user ${username}`)
+            user = await user_manager.create_user(username, password);
+        }
+        else {
+            throw e;
+        }
+    }
+
+    return user;
+}
+
+user_manager.get_leaderboard_data = async function() {
+    let leaderboard_data = {
+        us: {
+            taken: [
+
+            ],
+            average_score: [
+
+            ]
+        },
+
+        europe: {
+            taken: [
+
+            ],
+            average_score: [
+                
+            ]
+        },
+
+        china: {
+            taken: [
+
+            ],
+            average_score: [
+                
+            ]
+        },
+
+        all: {
+            taken: [
+
+            ],
+            average_score: [
+                
+            ]
+        }
+    }
+
+    let db_cursor = await user_collection.find({});
+    await db_cursor.forEach(user_doc => {
+        let quizzes = user_doc.quizResults;
+        let user_taken = {us: 0, europe: 0, china: 0};
+        let user_total_score = {us: 0.0, europe: 0.0, china: 0.0};
+
+        quizzes.forEach(quiz => {
+            switch (quiz.topic) {
+                case "US":
+                    user_taken.us += 1;
+                    user_total_score.us += quiz.score;
+                    break;
+                case "China":
+                    user_taken.china += 1;
+                    user_total_score.china += quiz.score;
+                    break;
+                case "EU":
+                    user_taken.europe += 1;
+                    user_total_score.europe += quiz.score;
+                    break;
+            }
+        });
+
+        leaderboard_data.us.taken.push({user: user_doc.username, value: user_taken.us});
+        leaderboard_data.us.average_score.push({user: user_doc.username, value: user_total_score.us / user_taken.us});
+
+        leaderboard_data.europe.taken.push({user: user_doc.username, value: user_taken.europe});
+        leaderboard_data.europe.average_score.push({user: user_doc.username, value: user_total_score.europe / user_taken.europe});
+
+        leaderboard_data.china.taken.push({user: user_doc.username, value: user_taken.china});
+        leaderboard_data.china.average_score.push({user: user_doc.username, value: user_total_score.china / user_taken.china});
+
+        leaderboard_data.all.taken.push({user: user_doc.username, value: user_taken.us + user_taken.china + user_taken.europe});
+        leaderboard_data.all.average_score.push({user: user_doc.username, value: (user_total_score.us + user_total_score.europe + user_total_score.china) / (user_taken.us + user_taken.europe + user_taken.china)});
+    });
+
+    return leaderboard_data;
 }
 
 module.exports = user_manager;
